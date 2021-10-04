2932296920edb43d6d17c3e58f565dc2b848da1d
Anthony VERCHERE <76996157+Anthony69600@users.noreply.github.com>
2021-10-04 12:23:02 +0200

    modifv1

--- thanks.php
+++ thanks.php
@@ -7,7 +7,7 @@
     <title>Document</title>
 </head>
 <body>
-    <p>Merci <?php echo $_POST['user_firstname'] . ' ' . $_POST['user_lastname'] ?> de nous avoir contacté à propos de “<?php echo $_POST['user_subject'] ?>”.
+    <p>Merci <?php echo $_POST['user_first_name'] . ' ' . $_POST['user_name'] ?> de nous avoir contacté à propos de “<?php echo $_POST['user_subject'] ?>”.
     Un de nos conseiller vous contactera soit à l’adresse <?php echo $_POST['user_mail'] ?> ou par téléphone au <?php echo $_POST['user_phone'] ?> dans les plus brefs délais pour traiter votre demande : 
     <?php echo $_POST['user_message'] ?>
     </p>
